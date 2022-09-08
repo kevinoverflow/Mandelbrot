f21324243b26dffdff98a25016f0423318d27081
Kevin Hoàng <kevinoverflow@gmail.com>
2022-09-08 15:51:14 +0200

    Add GLFW window hints

--- src/main.c
+++ src/main.c
@@ -1,17 +1,26 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include <stdio.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
 int main(int argc, char *argv[])
 {
     GLFWwindow *window;
-
+    
     if (!glfwInit())
+    {
+        printf("Failed to initialze GLFW\n");
         return -1;
-
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
     window = glfwCreateWindow(800, 600, "Hello, GLFW!", NULL, NULL);
     if (!window)
     {
+        printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
@@ -19,16 +28,23 @@ int main(int argc, char *argv[])
     glfwMakeContextCurrent(window);
     
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD\n");
+        glfwTerminate();
         return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.7f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
+    
     glfwTerminate();
     return 0;
 }
